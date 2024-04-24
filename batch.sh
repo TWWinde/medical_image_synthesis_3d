fd1537a072e98e1cac3dd489f1fc179f4edc0bce
TWWinde <twwinde@gmail.com>
2024-04-24 16:59:43 +0200

    update

--- batch.sh
+++ batch.sh
@@ -23,11 +23,11 @@ module load cuda
 # Run your python code
 
 #experiment_1 train VQ-GAN
-PL_TORCH_DISTRIBUTED_BACKEND=gloo python /misc/no_backups/s1449/medical_image_synthesis_3d/train/train_vqgan.py dataset=synthrad2023 \
-model=vq_gan_3d model.gpus=1 \
-model.default_root_dir_postfix='flair' model.precision=16 model.embedding_dim=8 \
-model.n_hiddens=16 model.downsample=[2,2,2] model.num_workers=32 model.gradient_clip_val=1.0 \
-model.lr=3e-4 model.discriminator_iter_start=10000 model.perceptual_weight=4 \
-model.image_gan_weight=1 model.video_gan_weight=1 model.gan_feat_weight=4 \
-model.batch_size=1 model.n_codes=16384 model.accumulate_grad_batches=1
+python /misc/no_backups/s1449/medical_image_synthesis_3d/train/train_vqgan.py dataset_name synthrad2023 \
+--name vq_gan_3d --gpu_ids 0,1 \
+ --precision 16 --embedding_dim 8 \
+--n_hiddens 16 --downsample [2,2,2] --num_workers 32 --gradient_clip_val 1.0 \
+--lr 0.0003 --discriminator_iter_start 10000 --perceptual_weight 4 \
+--image_gan_weight 1 --video_gan_weight 1 --gan_feat_weight 4 \
+--batch_size 1 --n_codes 16384 --accumulate_grad_batches 1
 

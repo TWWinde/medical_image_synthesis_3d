061bafc2d4a0bcb43904d270b625ec0f33c69863
TWWinde <twwinde@gmail.com>
2024-05-04 17:08:26 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -4,13 +4,12 @@
 #SBATCH --job-name=3DVQGAN
 #SBATCH --output=VQ-GAN%j.%N.out
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=2
+#SBATCH --cpus-per-task=4
 #SBATCH --time=6-23:00:00
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --qos=batch
-# SBATCH --nodes=1
 #SBATCH --gpus=rtx_a5000:1
 # SBATCH --gpus=geforce_rtx_2080ti:1
 # SBATCH --gpus=geforce_gtx_titan_x:1

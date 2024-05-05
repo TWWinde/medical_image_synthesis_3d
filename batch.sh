2913dbb99fa72335e48f435ad6e2c227096bdbb8
TWWinde <twwinde@gmail.com>
2024-05-05 20:06:23 +0200

    Update batch.sh

--- batch.sh
+++ batch.sh
@@ -7,10 +7,10 @@
 #SBATCH --cpus-per-task=4
 #SBATCH --time=6-23:00:00
 #SBATCH --mem=64G
-#SBATCH --nodes=2
+#SBATCH --nodes=1
 #SBATCH --gpus=2
 #SBATCH --qos=batch
-#SBATCH --gpus=rtx_a5000:1
+#SBATCH --gpus=rtx_a5000:0
 # SBATCH --gpus=geforce_rtx_2080ti:1
 # SBATCH --gpus=geforce_gtx_titan_x:1
 

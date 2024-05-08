f56cedef6e03490994f3fc19737c6f138aecaf75
TWWinde <twwinde@gmail.com>
2024-05-08 14:36:17 +0200

    Update tww.sh

--- tww.sh
+++ tww.sh
@@ -1,11 +1,20 @@
+#!/bin/bash -l
+
+#Slurm parameters
+#SBATCH --job-name=3DVQGAN
+#SBATCH --output=VQ-GAN%j.%N.out
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=4
+#SBATCH --time=6-23:00:00
 #SBATCH --mem=64G
 #SBATCH --nodes=1
 #SBATCH --gpus=1
 #SBATCH --qos=batch
-# SBATCH --gpus=rtx_a5000:1
+#SBATCH --gpus=rtx_a5000:1
 # SBATCH --gpus=rtx_a6000:1
 # SBATCH --gpus=geforce_rtx_2080ti:1
 # SBATCH --gpus=geforce_gtx_titan_x:1
+
 
 # Activate everything you need
 
